4b3e345e9062b77f9559399dadc379e74a7eadf7
linhnk <nguyenkhanhlinh2752001@gmail.com>
2022-08-05 09:19:37 +0700

    virtual properties & documents middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -129,7 +129,59 @@ exports.getTourStats = async(req, res) => {
         ])
         res.status(200).json({
             status: 'success',
+            totalResults: stats.length,
             data: { stats }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'failed',
+            message: error
+        })
+    }
+}
+
+exports.getMonthlyPlan = async(req, res) => {
+    try {
+        const year = req.params.year * 1
+        const plan = await Tour.aggregate([{
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numToursStart: { $sum: 1 },
+                    tours: { $push: '$name' }
+                }
+            },
+            {
+                $addFields: {
+                    month: '$_id'
+                }
+            },
+            {
+                $project: {
+                    _id: 0 // 0: not show id & 1: show id
+                }
+            },
+            {
+                $sort: { numToursStart: -1 }
+            },
+            {
+                $limit: 12
+            }
+        ])
+        res.status(200).json({
+            status: 'success',
+            totalResults: plan.length,
+            data: { plan }
         })
     } catch (error) {
         res.status(404).json({
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         trim: true,
         unique: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -34,12 +36,12 @@ const tourSchema = new mongoose.Schema({
     priceDiscount: Number,
     summary: {
         type: String,
-        trim: true
+        trim: true,
+        required: [true, 'A tour must have a summary']
     },
     description: {
         type: String,
-        trim: true,
-        required: [true, 'A tour must have a summary']
+        trim: true
     },
     imageCover: {
         type: String,
@@ -52,7 +54,30 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 })
+
+tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7
+})
+
+// DOCUMENTS MIDDLEWARE : runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
+// tourSchema.pre('save', function(next) {
+//     console.log('Will save document...')
+//     next()
+// })
+
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc)
+//     next()
+// })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
